0222cabdefe4bd8e91bce29b07b4057f37161ab0
jkotz <jhnkotz@gmail.com>
2015-08-03 22:31:17 -0500

    getting james hardens stuff

--- nbaShotCharts.py
+++ nbaShotCharts.py
@@ -8,8 +8,22 @@ import seaborn as sns
 
 sns.set_style('white')
 sns.set_color_codes()
-
-
+shot_chart_url = 'http://stats.nba.com/stats/shotchartdetail?CFID=33&CFPAR'\
+                'AMS=2014-15&ContextFilter=&ContextMeasure=FGA&DateFrom=&D'\
+                'ateTo=&GameID=&GameSegment=&LastNGames=0&LeagueID=00&Loca'\
+                'tion=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&'\
+                'PaceAdjust=N&PerMode=PerGame&Period=0&PlayerID=201935&Plu'\
+                'sMinus=N&Position=&Rank=N&RookieYear=&Season=2014-15&Seas'\
+                'onSegment=&SeasonType=Regular+Season&TeamID=0&VsConferenc'\
+                'e=&VsDivision=&mode=Advanced&showDetails=0&showShots=1&sh'\
+                'owZones=0'
+# Get the webpage containing the data
+response = requests.get(shot_chart_url)
+# Grab the headers to be used as column headers for our DataFrame
+headers = response.json()['resultSets'][0]['headers']
+# Grab the shot chart data
+shots = response.json()['resultSets'][0]['rowSet']
+print(shots)
 class NoPlayerError(Exception):
     """Custom Exception for invalid player search in get_player_id()"""
     def __init__(self, value):
